8bb16bf28372d0c0542fb1c026b2683c9340cecd
Juan Lo <lovedrones@gmail.com>
2023-05-10 11:33:50 -0500

    register form auth

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -34,7 +34,6 @@ export default {
   methods: {
     toggleAuthModal() {
       this.modalStore.isOpen = !this.modalStore.isOpen
-      console.log(this.modalStore.isOpen)
     }
   }
 }
---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDQmYvKW9LbQFFXkuBa_C4Bi7V1PIzNQTM',
+  authDomain: 'music-cc2ed.firebaseapp.com',
+  projectId: 'music-cc2ed',
+  storageBucket: 'music-cc2ed.appspot.com',
+  appId: '1:249455820117:web:a3788890cd55d8c7c8c3dc'
+}
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig)
+
+const auth = firebase.auth()
+const db = firebase.firestore()
+
+const usersCollection = db.collection('users')
+
+export { auth, db, usersCollection }
