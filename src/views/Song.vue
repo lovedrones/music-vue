83939d75986d2bc99254300bb37a25b40a6ed84e
Juan Lo <lovedrones@gmail.com>
2023-07-11 20:53:35 -0500

    song vue

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -29,7 +29,14 @@
         <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
       </div>
       <div class="p-6">
-        <vee-form :validation-schema="schema">
+        <div
+          class="text-white text-center font-bold p-4 mb-4"
+          v-if="comment_show_alert"
+          :class="comment_alert_variant"
+        >
+          {{ comment_alert_message }}
+        </div>
+        <vee-form :validation-schema="schema" @submit="addComment" v-if="userLoggedIn">
           <vee-field
             as="textarea"
             name="comment"
@@ -37,7 +44,11 @@
             placeholder="Your comment here..."
           ></vee-field>
           <errorMessage class="text-red-600" name="comment" />
-          <button type="submit" class="py-1.5 px-3 rounded text-white bg-green-600 block">
+          <button
+            type="submit"
+            class="py-1.5 px-3 rounded text-white bg-green-600"
+            :disabled="comment_in_submission"
+          >
             Submit
           </button>
         </vee-form>
@@ -129,7 +140,9 @@
 </template>
 
 <script>
-import { songsCollection } from '@/firebase'
+import { songsCollection, auth, commentsCollection } from '@/firebase'
+import { mapState } from 'pinia'
+import useUserStore from '@/stores/user'
 
 export default {
   name: 'Song',
@@ -138,8 +151,15 @@ export default {
       song: {},
       schema: {
         comment: 'required|min:3'
-      }
+      },
+      comment_in_submission: false,
+      comment_show_alert: false,
+      comment_alert_variant: 'bg-blue-500',
+      comment_alert_message: 'Please wait ! Your comment is being submitted'
     }
+  },
+  computed: {
+    ...mapState(useUserStore, ['userLoggedIn'])
   },
   async created() {
     const docSnapshot = await songsCollection.doc(this.$route.params.id).get()
@@ -150,6 +170,29 @@ export default {
     }
 
     this.song = docSnapshot.data()
+  },
+  methods: {
+    async addComment(values, { resetForm }) {
+      this.comment_in_submission = true
+      this.comment_show_alert = true
+      this.comment_alert_variant = 'bg-blue-500'
+      this.comment_alert_message = 'Please wait ! Your comment is being submitted'
+
+      const comment = {
+        content: values.comment,
+        datePosted: new Date().toString(),
+        sid: this.$route.params.id,
+        name: auth.currentUser.displayName,
+        uid: auth.currentUser.uid
+      }
+      await commentsCollection.add(comment)
+
+      this.comment_in_submission = false
+      this.comment_alert_variant = ' bg-green-500'
+      this.comment_alert_message = 'Comment added!'
+
+      resetForm()
+    }
   }
 }
 </script>
