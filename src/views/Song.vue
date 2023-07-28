55dc92f8029f24cc3b3348b15ea1d6a77024ad58
Juan Lo <lovedrones@gmail.com>
2023-07-27 22:07:33 -0500

    route transitions

--- src/views/Song.vue
+++ src/views/Song.vue
@@ -1,87 +1,87 @@
 <template>
-  <!-- Music Header -->
-  <section class="w-full mb-8 py-14 text-center text-white relative">
-    <div
-      class="absolute inset-0 w-full h-full box-border bg-contain music-bg"
-      style="background-image: url(/assets/img/song-header.png)"
-    ></div>
-    <div class="container mx-auto flex items-center">
-      <!-- Play/Pause Button -->
-      <button
-        @click.prevent="newSong(song)"
-        type="button"
-        class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
-      >
-        <i class="fas fa-play"></i>
-      </button>
-      <div class="z-50 text-left ml-8">
-        <!-- Song Info -->
-        <div class="text-3xl font-bold">{{ song.modified_name }}</div>
-        <div>{{ song.genre }}</div>
-      </div>
-    </div>
-  </section>
-  <!-- Form -->
-  <section class="container mx-auto mt-6">
-    <div class="bg-white rounded border border-gray-200 relative flex flex-col">
-      <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
-        <!-- Comment Count -->
-        <span class="card-title">Comments {{ song.comment_count }}</span>
-        <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
-      </div>
-      <div class="p-6">
-        <div
-          class="text-white text-center font-bold p-4 mb-4"
-          v-if="comment_show_alert"
-          :class="comment_alert_variant"
+  <main>
+    <!-- Music Header -->
+    <section class="w-full mb-8 py-14 text-center text-white relative">
+      <div
+        class="absolute inset-0 w-full h-full box-border bg-contain music-bg"
+        style="background-image: url(/assets/img/song-header.png)"
+      ></div>
+      <div class="container mx-auto flex items-center">
+        <!-- Play/Pause Button -->
+        <button
+          @click.prevent="newSong(song)"
+          type="button"
+          class="z-50 h-24 w-24 text-3xl bg-white text-black rounded-full focus:outline-none"
         >
-          {{ comment_alert_message }}
+          <i class="fas fa-play"></i>
+        </button>
+        <div class="z-50 text-left ml-8">
+          <!-- Song Info -->
+          <div class="text-3xl font-bold">{{ song.modified_name }}</div>
+          <div>{{ song.genre }}</div>
         </div>
-        <vee-form :validation-schema="schema" @submit="addComment">
-          <vee-field
-            as="textarea"
-            name="comment"
-            class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded mb-4"
-            placeholder="Your comment here..."
-          ></vee-field>
-          <errorMessage class="text-red-600" name="comment" />
-          <button
-            type="submit"
-            class="py-1.5 px-3 rounded text-white bg-green-600"
-            :disabled="comment_in_submission"
+      </div>
+    </section>
+    <!-- Form -->
+    <section class="container mx-auto mt-6" id="comments">
+      <div class="bg-white rounded border border-gray-200 relative flex flex-col">
+        <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+          <!-- Comment Count -->
+          <span class="card-title">Comments ({{ song.comment_count }})</span>
+          <i class="fa fa-comments float-right text-green-400 text-2xl"></i>
+        </div>
+        <div class="p-6">
+          <div
+            class="text-white text-center font-bold p-4 mb-4"
+            v-if="comment_show_alert"
+            :class="comment_alert_variant"
           >
-            Submit
-          </button>
-        </vee-form>
-        <!-- Sort Comments -->
-        <select
-          v-model="sort"
-          class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-        >
-          <option value="1">Latest</option>
-          <option value="2">Oldest</option>
-        </select>
+            {{ comment_alert_message }}
+          </div>
+          <vee-form :validation-schema="schema" @submit="addComment" v-if="userLoggedIn">
+            <vee-field
+              as="textarea"
+              name="comment"
+              class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded mb-4"
+              placeholder="Your comment here..."
+            ></vee-field>
+            <ErrorMessage class="text-red-600" name="comment" />
+            <button
+              type="submit"
+              class="py-1.5 px-3 rounded text-white bg-green-600 block"
+              :disabled="comment_in_submission"
+            >
+              Submit
+            </button>
+          </vee-form>
+          <!-- Sort Comments -->
+          <select
+            v-model="sort"
+            class="block mt-4 py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+          >
+            <option value="1">Latest</option>
+            <option value="2">Oldest</option>
+          </select>
+        </div>
       </div>
-    </div>
-  </section>
-  <!-- Comments -->
-  <ul class="container mx-auto">
-    <li
-      class="p-6 bg-gray-50 border border-gray-200"
-      v-for="comment in sortedComments"
-      :key="comment.docID"
-    >
-      <!-- Comment Author -->
-      <div class="mb-5">
-        <div class="font-bold">{{ comment.name }}</div>
-        <time>{{ comment.datePosted }}</time>
-      </div>
+    </section>
+    <!-- Comments -->
+    <ul class="container mx-auto">
+      <li
+        class="p-6 bg-gray-50 border border-gray-200"
+        v-for="comment in sortedComments"
+        :key="comment.docID"
+      >
+        <!-- Comment Author -->
+        <div class="mb-5">
+          <div class="font-bold">{{ comment.name }}</div>
+          <time>{{ comment.datePosted }}</time>
+        </div>
 
-      <p>
-        {{ comment.content }}
-      </p>
-    </li>
-  </ul>
+        <p>{{ comment.content }}</p>
+      </li>
+    </ul>
+  </main>
 </template>
 
 <script>
@@ -101,7 +101,7 @@ export default {
       comment_in_submission: false,
       comment_show_alert: false,
       comment_alert_variant: 'bg-blue-500',
-      comment_alert_message: 'Please wait ! Your comment is being submitted',
+      comment_alert_message: 'Please wait! Your comment is being submitted',
       comments: [],
       sort: '1'
     }
@@ -113,7 +113,8 @@ export default {
         if (this.sort === '1') {
           return new Date(b.datePosted) - new Date(a.datePosted)
         }
-        return new Date(b.datePosted) - new Date(a.datePosted)
+
+        return new Date(a.datePosted) - new Date(b.datePosted)
       })
     }
   },
@@ -138,7 +139,7 @@ export default {
       this.comment_in_submission = true
       this.comment_show_alert = true
       this.comment_alert_variant = 'bg-blue-500'
-      this.comment_alert_message = 'Please wait ! Your comment is being submitted'
+      this.comment_alert_message = 'Please wait! Your comment is being submitted'
 
       const comment = {
         content: values.comment,
@@ -147,6 +148,7 @@ export default {
         name: auth.currentUser.displayName,
         uid: auth.currentUser.uid
       }
+
       await commentsCollection.add(comment)
 
       this.song.comment_count += 1
@@ -157,7 +159,7 @@ export default {
       this.getComments()
 
       this.comment_in_submission = false
-      this.comment_alert_variant = ' bg-green-500'
+      this.comment_alert_variant = 'bg-green-500'
       this.comment_alert_message = 'Comment added!'
 
       resetForm()
@@ -167,12 +169,12 @@ export default {
 
       this.comments = []
 
-      snapshots.forEach((doc) => [
+      snapshots.forEach((doc) => {
         this.comments.push({
           docID: doc.id,
           ...doc.data()
         })
-      ])
+      })
     }
   },
   watch: {
@@ -180,6 +182,7 @@ export default {
       if (newVal === this.$route.query.sort) {
         return
       }
+
       this.$router.push({
         query: {
           sort: newVal
