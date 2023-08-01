6579196843df033c4020ec7be16af9577bff1d17
Juan Lo <lovedrones@gmail.com>
2023-08-01 18:03:57 -0500

    i18n

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App.vue'
 import router from './router'
 import VeeValidatePlugin from './validation'
 import { auth } from './firebase'
+import Icon from './directives/icon'
+import i18n from './i18n'
 
 import './assets/base.css'
 import './assets/main.css'
@@ -18,6 +20,8 @@ auth.onAuthStateChanged(() => {
     app.use(createPinia())
     app.use(router)
     app.use(VeeValidatePlugin)
+    app.use(i18n)
+    app.directive('icon', Icon)
 
     app.mount('#app')
   }
